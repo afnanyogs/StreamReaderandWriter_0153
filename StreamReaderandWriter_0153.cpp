deb1bd75747f52c55de827a0453e1f5c3dcbf24e
Afnan Asilah <afnanyogs@gmail.com>
2024-06-04 11:29:48 +0700

    menampilkan getline untuk mendapatkan setiap satu karakter dalam satu baris

--- StreamReaderandWriter_0153.cpp
+++ StreamReaderandWriter_0153.cpp
@@ -14,6 +14,11 @@ int main() {
 
 	cout << ">=Menulis file,\'q\' untuk kelas keluar" << endl;
 
-	
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "-";
+		//mendapatkan setiap karakter dalam satu baris
+	 	getline(cin, baris);
+		
 
 }
